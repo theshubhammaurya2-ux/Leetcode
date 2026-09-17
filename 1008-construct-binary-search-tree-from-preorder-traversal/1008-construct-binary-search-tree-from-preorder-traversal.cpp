84c97121dffae14b831c8c6bf1194a3f633e318d
theshubhammaurya2-ux <theshubhammaurya2@gmail.com>
2026-09-18 00:45:46 +0530

    Time: 0 ms (100%), Space: 15.9 MB (94.86%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,26 +10,32 @@
  * };
  */
 class Solution {
-public:  TreeNode* build(vector<int>& pre,int prelo,int prehi, vector<int>& in,int inlo,int inhi){
-           
-            if(prelo>prehi || inlo>inhi){return NULL;}
-             TreeNode* root=new TreeNode(pre[prelo]);
-            if(prelo==prehi) return root;
-            int i=inlo;
-            while(i<=inhi){
-                if(in[i]==pre[prelo]){ break;}
-                i++;
+public:  
+  TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){ TreeNode* temp=new TreeNode(val);root=temp;}
+
+        else if(root->val>val ){
+            if(root->left==NULL){
+                TreeNode* temp=new TreeNode(val);
+                root->left=temp;
             }
-            int leftcount=i-inlo;
-            int rightcount=inhi-i;
-            root->left=build(pre,prelo+1,prelo+leftcount ,in,inlo,i-1);
-            root->right=build(pre,prelo+leftcount+1,prehi ,in,i+1,inhi);
-            return root;
-     }
+            else{ insertIntoBST(root->left,val);}
+        }
+        else {
+            if(root->right==NULL){
+                TreeNode* temp=new TreeNode(val);
+                root->right=temp;
+            }
+            else{ insertIntoBST(root->right,val);}
+        }
+        return root;
+    }
     TreeNode* bstFromPreorder(vector<int>& pre) {
-        vector<int>in=pre;
-        sort(in.begin(),in.end());
-     int n=pre.size();
-        return build(pre,0,n-1,in,0,n-1);
+        TreeNode* root=new TreeNode(pre[0]);
+        for(int i=1;i<pre.size();i++){
+            insertIntoBST(root,pre[i]);
+
+        }
+        return root;
     }
 };
