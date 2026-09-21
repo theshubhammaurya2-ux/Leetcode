4952b78e75a73839423444d2d41aa9dd7507cd48
shubham maurya <theshubhammaurya2@gmail.com>
2026-09-21 19:58:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -8,18 +8,15 @@ public:
         map1[s[i]]++;
     }
       for(int i=0;i<t.length();i++){
-        map2[t[i]]++;
-    }
-    for(auto x:map1){
-        char ch1=x.first;
-        int freq1=x.second;
-        if(map2.find(ch1)!=map2.end()){
-            int freq2=map2[ch1];
-            if(freq1!=freq2) return false;
-
+        char ch=t[i];
+        if(map1.find(ch)!=map1.end()){
+          map1[ch]--;
+          if(map1[ch]==0) map1.erase(ch);
         }
-        else{return false;}
+        else return false;
+        
     }
+    if(map1.size()>0) return false;
     return true;
     }
 };
